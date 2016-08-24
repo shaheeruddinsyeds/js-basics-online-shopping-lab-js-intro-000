d6d3c81817a545d51ae8ea3f5f47b87921456c62
syed shaheeruddin <shaheeruddinsyeds@gmail.com>
2016-08-24 04:32:23 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function getCart(){
 }
 
 function addToCart(item){
-  cart.push(`${item}`);
 var price=Math.floor((Math.random() * 100) + 1);
 cart[`${item}`]=`${price}`;
 return (`${item}`+" has been added to your cart.");
